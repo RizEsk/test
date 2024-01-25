fc6ba0765fad33bf731bfa0f6d5d5d8d1f6305c9
RizEsk <Eskandiev@gmail.com>
2024-01-25 13:24:00 +0300

    wrong test 3

--- test.py
+++ test.py
@@ -1,16 +1,10 @@
-import pytest
-
-
-
 def first(x):
     return x + 1
 
-def test1():
-    assert first(3) == 4
-    
-def test2():
-    assert first(5) == 6
+def test(a, b):
+    assert first(a) == b
 
-def test3():
-    r= 5+1    
-    assert first(6) == r
+
+test(1, 2)
+test(3, 4)
+test(5, 5)
